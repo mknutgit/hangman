7ee6aafb690de89fb83d677bd97ba2b93fb2ca64
Josh Overly and matt knutson <mknut001@gmail.com>
2016-01-27 16:52:28 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+
+    var randomWord = function() {
+      var wordArray: ["cow" ];
+      var word = wordArray[Math.floor(Math.random() * wordArray.length)];
+      var wordSplit = word.split(" ");
+      return wordSplit
+}
+// var splitWord = function() {
+//   var words = ["cow"];
+//
+//   var word = words[Math.floor(Math.random() * words.length)];
+//   var wordLetters = word.split(" ");
+//   return wordLetters;
+// }
---
+++ spec/specs.js
@@ -0,0 +1,27 @@
+describe("randomWord", function() {
+  it("chooses a random word from array returns an array of it's letters", function() {
+    
+    expect(randomWord()to.eql("c", "o", "w");
+  });
+});
+
+
+
+//   it("is false if years is a negative number", function() {
+//     expect(leapYear(-2)).to.equal(false);
+//   });
+//
+//   it("is false if years is a letter", function() {
+//     expect(leapYear('a')).to.equal(false);
+//   });
+//   it("is false if years is a symbol", function() {
+//     expect(leapYear('@')).to.equal(false);
+//   });
+//
+// });
+// describe('splitWord', function() {
+//   it("returns the letters of a word in an array", function() {
+//   expect(splitWord()).to.be.a("array");
+//   });
+//
+// });
